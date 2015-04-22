7cc487c839eedf8bc0deec647f771f4634384822
freejoe76 <jmurphy@denverpost.com>
2015-04-22 13:33:38 -0600

    Update table structure

--- www/admin/tables.sql
+++ www/admin/tables.sql
@@ -1,24 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: localhost:8889
--- Generation Time: Apr 21, 2015 at 11:42 PM
--- Server version: 5.5.34
--- PHP Version: 5.5.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `mapgame`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `games`
---
+CREATE TABLE `guesses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `games_id` int(11) NOT NULL,
+  `guess` float NOT NULL,
+  `lat` float NOT NULL,
+  `lon` float NOT NULL,
+  `ip` varchar(30) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `games_id` (`games_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 CREATE TABLE `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,40 +22,7 @@ CREATE TABLE `games` (
   `lon` float NOT NULL,
   `ip` varchar(50) COLLATE utf8_bin NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `correct` int(11) NOT NULL DEFAULT '0',
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `slug` (`slug`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
-
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: localhost:8889
--- Generation Time: Apr 21, 2015 at 11:43 PM
--- Server version: 5.5.34
--- PHP Version: 5.5.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `mapgame`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `guesses`
---
-
-CREATE TABLE `guesses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `games_id` int(11) NOT NULL,
-  `guess` float NOT NULL,
-  `lat` float NOT NULL,
-  `lon` float NOT NULL,
-  `ip` varchar(30) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`games_id`),
-  UNIQUE KEY `id` (`id`),
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
