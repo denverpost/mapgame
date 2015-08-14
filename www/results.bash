334badb98d471fe6acb5fff45ccb6f33d15797c4
freejoe76 <joe.murphy@gmail.com>
2015-08-14 11:11:54 -0600

    help streamline results

--- www/results.bash
+++ www/results.bash
@@ -15,3 +15,4 @@ SQL="SELECT CONCAT(guess, ',', lat, ',', lon) FROM guesses WHERE games_id = $ID;
 echo $SQL > results.sql
 mysql mapgame -u root -p < results.sql >> results.tmp
 sed '2d' results.tmp >> results.csv
+echo 'http://denverpostplus.com/app/mapgame/www/results.csv'
