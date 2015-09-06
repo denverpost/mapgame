fdf2665f24dae7aa632813754634c029ca6094b2
freejoe76 <jmurphy@denverpost.com>
2015-09-06 18:23:54 -0400

    Start on a way to make edits to all the game subnavs quickly

---
+++ www/games/edit.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Remove a block of text and put another block of text in its place.
+# Blocks of text, as of now, are demarcated as such:
+# ###START-nameofblock
+# text etc. do what you want the rhythm's gonna get you
+# ###END-nameofblock
+#
+# The text that's inserted comes from a file.
+# The operation's performed on all unnamed arguments.
+
+BLOCKNAME=''
+FILE=''
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --blockname )
+            BLOCKNAME=$1
+            shift;;
+        -f | --file )
+            file=$1
+            shift;;
+    esac
+done
+
+# Remove the existing block
+sed -i "/###START-$BLOCKNAME/,/###END-$BLOCKNAME/d" 
+#    echo "###START-$BLOCKNAME" >> 
+#    cat
+#    echo '###END-blockit' >> 
